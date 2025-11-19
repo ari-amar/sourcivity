00c0da6eb4e597d8f2f445569302b3065126e87a
z-ogren <zpo1@pitt.edu>
2025-11-18 19:54:16 -0500

    add env file

--- app/test_client.py
+++ app/test_client.py
@@ -3,7 +3,7 @@ import httpx
 def test_search_parts():
 	url = "http://localhost:8000/api/search/parts"
 	payload = {
-		"query": "industrial motor 3-phase 400V 50Hz IP55 TEFC 15 kW 1500 rpm foot-mounted IEC frame 160L flange B5",
+		"query": "mass flow controller",
 		"location_filter": None
 	}
 	httpx.post(url, json=payload, timeout=120)
